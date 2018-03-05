e8cb20407763a51e6cf3b11826c3a5543b1f35e3
Donald Venus <donaldvenus120@gmail.com>
2018-03-04 19:24:49 -0500

    Remove unnecessary files and rename processor back to processor.v

--- processor.v
+++ processor.v
@@ -44,6 +44,7 @@
 `define Start    5'b11111
 `define Start1   5'b11110
 `define OPjumpf2 5'b11101
+`define OPnop    5'b11100
 
 module processor(halt, reset, clk);
 output reg halt;
@@ -69,128 +70,178 @@ always @(posedge clk) begin
 	case (s)
 	    `Start:
 	    	begin
+	    		$display("Start\n");
 	    		ir <= mainmem[pc]; s <= `Start1;
 	    	end
 	    `Start1:
 	    	begin
-	    		pc <= pc + 1;
-	    		case (ir `OPCODE)
-	    			`OPnoarg:
-	    				s <= { 1'b1, ir `SECOP };
-	    			`OPtwoarg:
-	    				s <= { 1'b1, ir `SECOP };
-	    			default:
-	    				s <= ir `OPCODE;
-	    		endcase
+	    		$display("Start1\n");
+		    	case (enstack[0])
+		    		1:
+			    		begin
+							pc <= pc + 1;
+							case (ir `OPCODE)
+								`OPnoarg: begin
+									s <= { 1'b1, ir `SECOP };
+									end
+								`OPtwoarg: begin
+									s <= { 1'b1, ir `SECOP };
+									end
+								default: begin
+									s <= ir `OPCODE;
+									end
+							endcase
+						end
+
+		    		0:
+			    		begin
+			    			pc <= pc + 1;
+							case (ir `OPCODE)
+								`OPjump: s <= ir `OPCODE;
+								`OPjumpf: s <= ir `OPCODE;
+								`OPcall: s <= ir `OPCODE;
+								`OPnoarg: begin
+									s <= { 1'b1, ir `SECOP };
+									end
+								default: s <= `OPnop;
+							endcase
+						end
+				endcase
+	    	end
+	    `OPnop:
+	    	begin
+	    		$display("nop\n");
+	    		s <= `Start;
 	    	end
 	    `OPadd:
 	    	begin
 	    		$display("add\n");
-                        regfile[ir `DEST] <= regfile[ir `SRC] + regfile[ir `SECOP];
+                regfile[ir `DEST] <= regfile[ir `SRC] + regfile[ir `SECOP];
 	    		s <= `Start;
 	    	end
 	    `OPand:
 	    	begin
+	    		$display("and\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC] & regfile[ir `SECOP];
 	    		s <= `Start;
 	    	end
 	    `OPmul:
 	    	begin
+	    		$display("mul\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC] * regfile[ir `SECOP];
 	    		s <= `Start;
 	    	end
 	    `OPor:
 	    	begin
+	    		$display("or\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC] | regfile[ir `SECOP];
 	    		s <= `Start;
 	    	end
 	    `OPsll:
 	    	begin
+	    		$display("sll\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC] << (regfile[ir `SECOP] & 15);
 	    		s <= `Start;
 	    	end
 	    `OPslt:
 	    	begin
+	    		$display("slt\n");
 	    		regfile[ir `DEST] <= (regfile[ir `SRC] < regfile[ir `SECOP]);
 	    		s <= `Start;
 	    	end
 	    `OPsra:
 	    	begin
-	    		regfile[ir `DEST] <= regfile[ir `SRC] >> (regfile[ir `SECOP] & 15);
+	    		$display("sra\n");
+	    		regfile[ir `DEST] <= $signed(regfile[ir `SRC]) >>> (regfile[ir `SECOP] & 15);
 	    		s <= `Start;
 	    	end
 	    `OPxor:
 	    	begin
+	    		$display("xor\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC] ^ regfile[ir `SECOP];
 	    		s <= `Start;
 	    	end
 	    `OPli8:
 	    	begin
-	    		regfile[ir `DEST] <= { {8{ir `IMMED}}, ir `IMMED };
+	    		$display("li8\n");
+	    		regfile[ir `DEST] <= { {8{ir[7]}}, ir `IMMED };
 	    		s <= `Start;
 	    	end
 	    `OPlu8:
 	    	begin
+	    		$display("lu8\n");
 	    		regfile[ir `DEST] <= (regfile[ir `DEST] & 255) | (ir `IMMED << 8);
 	    		s <= `Start;
 	    	end
 	    `OPgor:
 	    	begin
+	    		$display("gor\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC];
 	    		s <= `Start;
 	    	end
 	    `OPleft:
 	    	begin
+	    		$display("left\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC];
 	    		s <= `Start;
 	    	end
 	    `OPlnot:
 	    	begin
+	    		$display("lnot\n");
 	    		regfile[ir `DEST] <= ~regfile[ir `SRC];
 	    		s <= `Start;
 	    	end
 	    `OPload:
 	    	begin
+	    		$display("load\n");
 	    		regfile[ir `DEST] <= mainmem[regfile[ir `SRC]];
 	    		s <= `Start;
 	    	end
 	    `OPneg:
 	    	begin
+	    		$display("neg\n");
 	    		regfile[ir `DEST] <= -regfile[ir `SRC];
 	    		s <= `Start;
 	    	end
 	    `OPright:
 	    	begin
+	    		$display("right\n");
 	    		regfile[ir `DEST] <= regfile[ir `SRC];
 	    		s <= `Start;
 	    	end
 	    `OPstore:
 	    	begin
+	    		$display("store\n");
 	    		mainmem[regfile[ir `SRC]] <= regfile[ir `DEST];
 	    		s <= `Start;
 	    	end
 	    `OPcall:
 	    	begin
+	    		$display("call\n");
 	    		if (enstack[0] == 1)
 	    			begin
 	    				callstack <= { callstack[47:0], pc };
 	    				pc <= mainmem[pc];
 	    			end
+	    		else pc <= pc + 1;
 	    		s <= `Start;
 	    	end
 	    `OPjump:
 	    	begin
+	    		$display("jump\n");
 	    		pc <= mainmem[pc];
 	    		s <= `Start;
 	    	end
 	    `OPjumpf:
 	    	begin
+	    		$display("jumpf\n");
 	    		if (regfile[ir `DEST] == 0) enstack <= (enstack & ~1);
 	    		s <= `OPjumpf2;
 	    	end
 	    `OPjumpf2:
 	    	begin
 	    		if (enstack[0] == 0) pc <= mainmem[pc];
+	    		else pc <= pc + 1;
 	    		s <= `Start;
 	    	end
 	    `OPtrap:
@@ -200,31 +251,36 @@ always @(posedge clk) begin
 	    	end
 	    `OPret:
 	    	begin
+	    		$display("ret\n");
 	    		if (enstack[0] == 1)
 	    			begin
-	    				pc <= callstack[15:0];
+	    				pc <= callstack[15:0] + 1;
 	    				callstack <= callstack >> 16;
 	    			end
 	    		s <= `Start;
 	    	end
 	    `OPallen:
 	    	begin
+	    		$display("allen\n");
 	    		enstack <= (enstack | 1);
 	    		s <= `Start;
 	    	end
 	    `OPpopen:
 	    	begin
-	    		enstack <= (enstack >> 1);
+	    		$display("popen\n");
+	    		enstack <= { 1'b1, enstack[31:1] };
 	    		s <= `Start;
 	    	end
 	    `OPpushen:
 	    	begin
+	    		$display("pushen\n");
 	    		enstack <= ((enstack << 1) | (enstack & 1));
 	    		s <= `Start;
 	    	end
 	    default: halt <= 1;
 	endcase
-end
+	end
+	
 endmodule
 
 module testbench;
@@ -234,7 +290,7 @@ wire halted;
 processor PE(halted, reset, clk);
 initial begin
   $dumpfile;
-  $dumpvars(0, PE);
+  $dumpvars;
   #10 reset = 1;
   #10 reset = 0;
   while (!halted) begin
